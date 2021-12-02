03d926417de2f22f15af0b02a9e28d611aa8f5a3
Primrose <primrose@noxxprojects.com>
2021-12-02 20:44:07 +0000

    some cleanup

--- MPMCQueue.h
+++ MPMCQueue.h
@@ -1,27 +1,54 @@
 #ifndef MPMCQUEUE_H
 #define MPMCQUEUE_H
 
-#include "Global.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+#include <atomic>
+#include <vector>
+
+typedef signed char        int8;
+typedef short              int16;
+typedef int                int32;
+typedef long long          int64;
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+
+#define PLATFORM_CACHE_LINE_SIZE 64
+#define MPMC_ALIGNMENT alignas(PLATFORM_CACHE_LINE_SIZE)
+#define MPMC_PADDING MPMC_ALIGNMENT uint8
+
+#define SPIN_WAIT_COUNT 1000
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+#define FORCEINLINE __forceinline
+#else
+#define FORCEINLINE inline
+#endif
+
+/**
+ * utility template for a class that should not be copyable.
+ * Derive from this class to make your class non-copyable
+ */
+class FNoncopyable
+{
+    /** @cite Taken from the Unreal Engine 4 Source code.
+     *  @ref <FileName> UnrealTemplate.h
+     */
+    
+    protected:
+    // ensure the class cannot be constructed directly
+    FNoncopyable() {}
+    // the class should not be used in a polymorphic manner
+    ~FNoncopyable() {}
+private:
+    FNoncopyable(const FNoncopyable&);
+    FNoncopyable& operator=(const FNoncopyable&);
+};
 
 #define SEQUENCE_ERROR_VALUE -2
-
-class UMPMCQueueMemoryStatics final : public FNoncopyable
-{
-public:
-    static FORCEINLINE void ReleaseFence()			noexcept	{ std::atomic_thread_fence(std::memory_order_release); }
-    static FORCEINLINE void AcquireFence()			noexcept	{ std::atomic_thread_fence(std::memory_order_acquire); }
-    static FORCEINLINE void FullFence()				noexcept	{ std::atomic_thread_fence(std::memory_order_seq_cst); }
-    static FORCEINLINE void AcquireReleaseFence()	noexcept	{ std::atomic_thread_fence(std::memory_order_acq_rel); }
-    
-    /** Contiguous memory allocation. All elements initialised to zero. */
-    template<typename TypeToAllocate>
-    static FORCEINLINE TypeToAllocate* MallocZeroed(const int64 NumberOfElements)
-    {
-        if(NumberOfElements == 0) return nullptr;
-
-        return (TypeToAllocate*)calloc(NumberOfElements, sizeof(TypeToAllocate));
-    }
-};
 
 template <typename T>
 class MPMC_ALIGNMENT TSequentialContainer
@@ -41,21 +68,21 @@ public:
     FORCEINLINE T Get() const
     {
         const T OutCopy = Data.load(std::memory_order_relaxed);
-        UMPMCQueueMemoryStatics::AcquireFence();
+        std::atomic_thread_fence(std::memory_order_acquire);
         return OutCopy;
     }
 
     FORCEINLINE void Set(const T& NewData)
     {
-        UMPMCQueueMemoryStatics::ReleaseFence();
+        std::atomic_thread_fence(std::memory_order_release);
         Data.store(NewData, std::memory_order_relaxed);
     }
 
     FORCEINLINE void SetVolatile(const T& NewData)
     {
-        UMPMCQueueMemoryStatics::ReleaseFence();
+        std::atomic_thread_fence(std::memory_order_release);
         Data.store(NewData, std::memory_order_relaxed);
-        UMPMCQueueMemoryStatics::FullFence();
+        std::atomic_thread_fence(std::memory_order_seq_cst);
     }
 
     FORCEINLINE bool CompareAndSet(T& Expected, const T& NewValue)
@@ -66,7 +93,7 @@ public:
     
 protected:
     MPMC_PADDING PadToAvoidContention0[PLATFORM_CACHE_LINE_SIZE] = { };
-    TMPMCAtomic<T> Data;
+    std::atomic<T> Data;
     MPMC_PADDING PadToAvoidContention1[PLATFORM_CACHE_LINE_SIZE] = { };
 };
 
@@ -100,16 +127,16 @@ class TMPMCQueue final : public FNoncopyable
 {
 private:
     using FElementType = T;
-
+    
 public:
     explicit TMPMCQueue()
     {
-        RingBuffer = UMPMCQueueMemoryStatics::MallocZeroed<FElementType>(TQueueSize);
+        RingBuffer = MallocZeroed();
         
         ConsumerCursor.SetVolatile(0);
         ProducerCursor.SetVolatile(0);
     }
-
+    
     bool Enqueue(const FElementType& NewElement)
     {
         const int64 CurrentConsumerCursor = ConsumerCursor.Get();
@@ -121,7 +148,7 @@ public:
             return false;
         }
         
-        int64 ClaimedIndex = ProducerCursor.Get();
+        int64 ClaimedIndex = CurrentProducerCursor;
         int64 NewIndex = ClaimedIndex + 1;
         
         while(!ProducerCursor.CompareAndSet(ClaimedIndex, NewIndex)) // Spin wait for cursor to update
@@ -170,6 +197,12 @@ private:
     FORCEINLINE int64 CalculateIndex(const uint64 IndexValue) const noexcept
     {
         return IndexValue & GetIndexMask();
+    }
+
+    /** Contiguous memory allocation. All elements initialised to zero. */
+    FORCEINLINE FElementType* MallocZeroed()
+    {
+        return (FElementType*)calloc(TQueueSize, sizeof(FElementType));
     }
     
 private:
